ba0c3075add7c556c1c80c407d8b52922811ad1e
Preston Chaplin <preston.chaplin@gmail.com>
2021-03-02 17:23:05 -0500

    separate rooms for doctor, NUC, Hololens

--- index.js
+++ index.js
@@ -1,56 +1,142 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 app.use(express.static(__dirname + '/public'));
+const http = require('http').createServer(app);
 
-var http = require('http').createServer(app);
-// var io = require('socket.io')(http, {allowEIO3: true, allowEIO4: true, serveClient: true});
-
-var io = require('socket.io')(http, {
+const io = require('socket.io')(http, {
   allowEIO3: true,
   allowEIO4: true,
   serveClient: true,
-  cors: { origin: '*'}
+  cors: { origin: '*' },
 });
 
-http.listen(3000, function(){ console.log('listening on *:3000');});
+http.listen(3000, function () {
+  console.log('listening on *:3000');
+});
 
-var serverID = 'undefined';
-io.on('connection', function (socket){
-    console.log('a user connected: ' + socket.id + " (server: " + serverID + " )");
+let serverID = 'undefined';
+
+const hololensFeed = io.of('/holo');
+
+hololensFeed.on('connection', function (socket) {
+    console.log('a Hololens user connected: ' + socket.id + ' (server: ' + serverID + ' )');
     //register the server id, received the command from unity
-    socket.on('RegServerId', function (data){
-        serverID = socket.id;
-        console.log('reg server id : ' + serverID);
+    socket.on('RegServerId', function (data) {
+      serverID = socket.id;
+      console.log('reg server id : ' + serverID);
     });
 
-    socket.on('disconnect', function(){
-        if (serverID == socket.id)
-        {
-           serverID = 'undefined';
-           console.log('removed Server: ' + socket.id);
-        }
-        else
-        {
-           console.log('user disconnected: ' + socket.id);
-        }
+    socket.on('disconnect', function () {
+      if (serverID == socket.id) {
+        serverID = 'undefined';
+        console.log('removed Server: ' + socket.id);
+      } else {
+        console.log('Hololens user disconnected: ' + socket.id);
+      }
     });
 
-    socket.on('OnReceiveData', function (data){
-        if (serverID != 'undefined')
-        {
-            switch(data.EmitType)
-            {
-                //emit type: all;
-                case 0: io.emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte }); break;
-                //emit type: server;
-                case 1: io.to(serverID).emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte }); break;
-                //emit type: others;
-                case 2: socket.broadcast.emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte }); break;
-            }
+    socket.on('OnReceiveData', function (data) {
+      if (serverID != 'undefined') {
+        switch (data.EmitType) {
+          //emit type: all;
+          case 0:
+            io.emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte });
+            break;
+          //emit type: server;
+          case 1:
+            io.to(serverID).emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte });
+            break;
+          //emit type: others;
+          case 2:
+            socket.broadcast.emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte });
+            break;
         }
-        else
-        {
-            console.log('cannot find any active server');
-        }
+      } else {
+        console.log('cannot find any active server');
+      }
     });
-});
+  });
+
+
+const nucFeed = io.of('/nuc');
+
+nucFeed.on('connection', function (socket) {
+    console.log('NUC connected: ' + socket.id + ' (server: ' + serverID + ' )');
+    //register the server id, received the command from unity
+    socket.on('RegServerId', function (data) {
+      serverID = socket.id;
+      console.log('reg server id : ' + serverID);
+    });
+
+    socket.on('disconnect', function () {
+      if (serverID == socket.id) {
+        serverID = 'undefined';
+        console.log('removed Server: ' + socket.id);
+      } else {
+        console.log('NUC disconnected: ' + socket.id);
+      }
+    });
+
+    socket.on('OnReceiveData', function (data) {
+      if (serverID != 'undefined') {
+        switch (data.EmitType) {
+          //emit type: all;
+          case 0:
+            io.emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte });
+            break;
+          //emit type: server;
+          case 1:
+            io.to(serverID).emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte });
+            break;
+          //emit type: others;
+          case 2:
+            socket.broadcast.emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte });
+            break;
+        }
+      } else {
+        console.log('cannot find any active server');
+      }
+    });
+  });
+
+  const doctorFeed = io.of('/doctor');
+
+doctorFeed.on('connection', function (socket) {
+    console.log('NUC connected: ' + socket.id + ' (server: ' + serverID + ' )');
+    //register the server id, received the command from unity
+    socket.on('RegServerId', function (data) {
+      serverID = socket.id;
+      console.log('reg server id : ' + serverID);
+    });
+
+    socket.on('disconnect', function () {
+      if (serverID == socket.id) {
+        serverID = 'undefined';
+        console.log('removed Server: ' + socket.id);
+      } else {
+        console.log('NUC disconnected: ' + socket.id);
+      }
+    });
+
+    socket.on('OnReceiveData', function (data) {
+      if (serverID != 'undefined') {
+        switch (data.EmitType) {
+          //emit type: all;
+          case 0:
+            io.emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte });
+            break;
+          //emit type: server;
+          case 1:
+            io.to(serverID).emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte });
+            break;
+          //emit type: others;
+          case 2:
+            socket.broadcast.emit('OnReceiveData', { DataString: data.DataString, DataByte: data.DataByte });
+            break;
+        }
+      } else {
+        console.log('cannot find any active server');
+      }
+    });
+  });
+
